53e7e799fdf76f43c45955d559bb5a3ff839c6c0
vilvaathibanpb <vilvaathiban@gmail.com>
2024-01-24 02:21:50 +0100

    minor fix

--- src/ProfileList.js
+++ src/ProfileList.js
@@ -1,28 +1,38 @@
 // ProfileList.js
-import React from 'react';
+import React from "react";
 
 const ProfileList = ({ profiles }) => {
   return (
     <div>
-      <h2 className="text-2xl font-bold mb-4 text-yellow-500 mb-10">Profile List</h2>
+      <h2 className="text-2xl font-bold mb-4 text-yellow-500 mb-10">
+        Profile List
+      </h2>
       <ul className="list-disc">
         {profiles.map((profile, index) => (
           <li key={index} className="mb-10">
-            <div className='flex justify-between items-center w-full md:w-1/2'>
-
-            <h3 className="text-xl font-semibold">{profile.name.toUpperCase()}</h3>
-            <div className="flex items-center space-x-2">
-              <a
-                href={profile.url}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-orange-500 hover:underline flex"
-              >
-                <img alt="linkedin logo" height={25} width={25} className='mr-4' src='https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/2048px-LinkedIn_icon.svg.png' />LinkedIn Profile
-              </a>
+            <div className="flex justify-between items-center w-full md:w-1/2">
+              <h3 className="text-xl font-semibold">
+                {profile.name.toUpperCase()}
+              </h3>
+              <div className="flex items-center space-x-2">
+                <a
+                  href={profile.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-orange-500 hover:underline flex md:flex-row flex-col justify-center items-center"
+                >
+                  <img
+                    alt="linkedin logo"
+                    height={25}
+                    width={25}
+                    className="mr-4"
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/2048px-LinkedIn_icon.svg.png"
+                  />
+                  <p>LinkedIn Profile</p>
+                </a>
+              </div>
             </div>
-            </div>
-            <p className="text-green-400 font-bold">{profile.position}</p>
+            <p className="text-green-400 font-bold mt-4">{profile.position}</p>
             <p className="text-pink-400 font-bold">{profile.location}</p>
           </li>
         ))}
